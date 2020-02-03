39bf12eb60a861885442647e1a61d6a15ef998c8
Michael Irvine <michaelirvinedesign@gmail.com>
2020-02-03 15:34:30 -0800

    changes to git ignore

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@
 export default function index() {
   return (
     <div className="home _wrapper">
-      <p>Michael Irvine is a creative front end developer based in Vancouver, Canada.</p>
+      <p>Michael Irvine is a creative front end developer and UI designer </p>
     </div>
   )
 }
